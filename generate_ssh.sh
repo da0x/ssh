bd40b830b6965dcf23da004cb0fd21e5fee85473
Daher Alfawares <daher.alfawares@live.com>
2018-07-18 09:20:36 -0500

    adding key script

--- generate_ssh.sh
+++ generate_ssh.sh
@@ -1,3 +1,8 @@
+echo Setting git config:
+
+git config --global user.email "daher.alfawares@live.com"
+git config --global user.name "Daher Alfawares"
+
 ssh-keygen -t rsa -b 4096 -C "daher.alfawares@live.com"
 eval $(ssh-agent -s)
 ssh-add ~/.ssh/id_rsa
