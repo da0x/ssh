8d4da90e34b006ace2810098cf7c5ed864a06dc2
Daher Alfawares <daher.alfawares@live.com>
2018-07-19 09:37:08 -0500

    Update generate_ssh.sh

--- generate_ssh.sh
+++ generate_ssh.sh
@@ -1,9 +1,13 @@
 echo Setting git config:
 
-git config --global user.email "daher.alfawares@live.com"
-git config --global user.name "Daher Alfawares"
+export GITHUB_NAME="Daher Alfawares"
+export GITHUB_EMAIL="daher.alfawares@live.com"
 
-ssh-keygen -t rsa -b 4096 -C "daher.alfawares@live.com"
+echo changing git config to $GITHUB_NAME - $GITHUB_EMAIL
+git config --global user.email $GITHUB_EMAIL 
+git config --global user.name $GITHUB_NAME
+
+ssh-keygen -t rsa -b 4096 -C $GITHUB_EMAIL
 eval $(ssh-agent -s)
 ssh-add ~/.ssh/id_rsa
 
